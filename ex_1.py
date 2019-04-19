579eafebad56c8aff406214e190d0874947ead10
tonybruscino <tonyrb12@gmail.com>
2019-04-19 22:02:14 +0200

    new changes

---
+++ ex_1.py
@@ -0,0 +1,33 @@
+# My first program; example 1
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# My second program; example 2
+print "I will now count my chickens:"
+# remember % means: a / b and the remainder is:  %(c)
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh thats why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ ex_6.py
@@ -0,0 +1,35 @@
+# My fourth program; example 6
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
+
+# My fifth program; example 8
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
